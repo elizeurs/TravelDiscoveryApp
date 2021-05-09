99247334184df2b411b4c5b3ce3c07917ce5858f
elizeurs <13ders@gmail.com>
2021-05-09 11:48:14 -0300

    navigationView, scrollViews and stacking

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    NavigationView {
+      
+      ScrollView {
+        
+        DiscoveryCategoriesView()
+        
+      }.navigationTitle("Discovery")
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+struct DiscoveryCategoriesView: View {
+  var body: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      HStack(spacing: 8) {
+        ForEach(0 ..< 5) { num in
+          VStack(spacing: 4) {
+            Spacer()
+              .frame(width: 50, height: 50)
+              .background(Color.gray)
+              .cornerRadius(.infinity)
+              .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+            Text("Art")
+              .font(.system(size: 12, weight: .semibold))
+          }
+        }
+      }.padding(.horizontal)
     }
+  }
 }
