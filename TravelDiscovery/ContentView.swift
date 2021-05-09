08613643848bbbd1559191239b4ee2b0c7117c8a
elizeurs <13ders@gmail.com>
2021-05-09 15:24:09 -0300

    forEach, hashables and system images

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -15,7 +15,97 @@ struct ContentView: View {
         
         DiscoveryCategoriesView()
         
+        PopularDestinationsView()
+        
+        PopularRestaurantsView()
+        
+        TrendingCreatorsView()
+        
       }.navigationTitle("Discovery")
+    }
+  }
+}
+
+struct PopularDestinationsView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        Text("Popular Destinations")
+          .font(.system(size: 14, weight: .semibold))
+        Spacer()
+        Text("See All")
+          .font(.system(size: 12, weight: .semibold))
+      }.padding(.horizontal)
+      .padding(.top)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 8.0) {
+          ForEach(0 ..< 5, id: \.self) { num in
+            Spacer()
+              .frame(width: 125, height: 150)
+              .background(Color.gray)
+              .cornerRadius(5)
+              .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+              .padding(.bottom)
+          }
+        }.padding(.horizontal)
+      }
+    }
+  }
+}
+
+struct PopularRestaurantsView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        Text("Popular Places To Eat")
+          .font(.system(size: 14, weight: .semibold))
+        Spacer()
+        Text("See All")
+          .font(.system(size: 12, weight: .semibold))
+      }.padding(.horizontal)
+      .padding(.top)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 8.0) {
+          ForEach(0 ..< 5, id: \.self) { num in
+            Spacer()
+              .frame(width: 200, height: 64)
+              .background(Color.gray)
+              .cornerRadius(5)
+              .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+              .padding(.bottom)
+          }
+        }.padding(.horizontal)
+      }
+    }
+  }
+}
+
+struct TrendingCreatorsView: View {
+  var body: some View {
+    VStack {
+      HStack {
+        Text("Trending Creators")
+          .font(.system(size: 14, weight: .semibold))
+        Spacer()
+        Text("See All")
+          .font(.system(size: 12, weight: .semibold))
+      }.padding(.horizontal)
+      .padding(.top)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(spacing: 8.0) {
+          ForEach(0 ..< 5, id: \.self) { num in
+            Spacer()
+              .frame(width: 50, height: 50)
+              .background(Color.gray)
+              .cornerRadius(.infinity)
+              .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+              .padding(.bottom)
+          }
+        }.padding(.horizontal)
+      }
     }
   }
 }
@@ -26,20 +116,37 @@ struct ContentView_Previews: PreviewProvider {
   }
 }
 
+struct Category: Hashable {
+  let name, imageName: String
+}
+
 struct DiscoveryCategoriesView: View {
+  
+  let categories: [Category] = [
+    .init(name: "Art", imageName: "paintpalette.fill"),
+    .init(name: "Sport", imageName: "sportscourt.fill"),
+    .init(name: "Live Events", imageName: "music.mic"),
+    .init(name: "Food", imageName: "music.mic"),
+    .init(name: "History", imageName: "music.mic"),
+  ]
+  
   var body: some View {
     ScrollView(.horizontal, showsIndicators: false) {
-      HStack(spacing: 8) {
-        ForEach(0 ..< 5) { num in
-          VStack(spacing: 4) {
-            Spacer()
-              .frame(width: 50, height: 50)
+      HStack(alignment: .top, spacing: 14) {
+        ForEach(categories, id: \.self) { category in
+          VStack(spacing: 8) {
+//            Spacer()
+            Image(systemName: category.imageName)
+              .font(.system(size: 20))
+              .foregroundColor(.white)
+              .frame(width: 68, height: 68)
               .background(Color.gray)
-              .cornerRadius(.infinity)
+              .cornerRadius(34)
               .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
-            Text("Art")
+            Text(category.name)
               .font(.system(size: 12, weight: .semibold))
-          }
+              .multilineTextAlignment(.center)
+          }.frame(width: 68)
         }
       }.padding(.horizontal)
     }
