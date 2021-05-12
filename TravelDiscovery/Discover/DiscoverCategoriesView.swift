60020135603766289743875a31b88998f54f2e76
elizeurs <13ders@gmail.com>
2021-05-12 13:01:56 -0300

    Using NavigationLink to Perform Transitions

--- TravelDiscovery/Discover/DiscoverCategoriesView.swift
+++ TravelDiscovery/Discover/DiscoverCategoriesView.swift
@@ -21,30 +21,61 @@ struct DiscoverCategoriesView: View {
     ScrollView(.horizontal, showsIndicators: false) {
       HStack(alignment: .top, spacing: 14) {
         ForEach(categories, id: \.self) { category in
-          VStack(spacing: 8) {
-            //            Spacer()
-            Image(systemName: category.imageName)
-              .font(.system(size: 25))
-              .foregroundColor(Color(#colorLiteral(red: 0.9989508986, green: 0.4407047629, blue: 0, alpha: 1)))
-              .frame(width: 68, height: 68)
-              .background(Color.white)
-              .cornerRadius(34)
-            //              .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
-            Text(category.name)
-              .font(.system(size: 12, weight: .semibold))
-              .multilineTextAlignment(.center)
-          }.frame(width: 68)
+          NavigationLink(
+            destination: CategoryDetailsView(),
+            label: {
+              VStack(spacing: 8) {
+                //            Spacer()
+                Image(systemName: category.imageName)
+                  .font(.system(size: 25))
+                  .foregroundColor(Color(#colorLiteral(red: 0.9989508986, green: 0.4407047629, blue: 0, alpha: 1)))
+                  .frame(width: 68, height: 68)
+                  .background(Color.white)
+                  .cornerRadius(34)
+                //              .shadow(color: .gray, radius: 4, x: 0.0, y: 2)
+                Text(category.name)
+                  .font(.system(size: 12, weight: .semibold))
+                  .multilineTextAlignment(.center)
+                  .foregroundColor(.white)
+              }.frame(width: 68)
+            })
         }
       }.padding(.horizontal)
     }
   }
 }
 
+struct CategoryDetailsView: View {
+  var body: some View {
+    ScrollView {
+        ForEach(0 ..< 5, id: \.self) { item in
+          VStack(alignment: .leading, spacing: 0) {
+          Image("art1")
+            .resizable()
+            .scaledToFill()
+          
+          Text("Demo 123")
+            .font(.system(size: 12, weight: .semibold))
+            .padding()
+        }.asTile()
+        .padding()
+      }
+    }.navigationBarTitle("Category", displayMode: .inline)
+  }
+}
+
 struct DiscoverCategoriesView_Previews: PreviewProvider {
     static var previews: some View {
-      ZStack {
-        Color(#colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1))
-        DiscoverCategoriesView()
+      
+      NavigationView {
+        CategoryDetailsView()
       }
+      
+      DiscoverView()
+      
+//      ZStack {
+//        Color(#colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1))
+//        DiscoverCategoriesView()
+//      }
     }
 }
